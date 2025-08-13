74ca51df4cd6874428afd5aceaa260e1f5ad0663
Saverio976 <xavier.mitault@epitech.eu>
2025-08-13 22:58:09 +0100

    fix

--- debian_install_hugo.sh
+++ debian_install_hugo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -Lo /tmp/hugo.deb 'https://github.com/gohugoio/hugo/releases/download/v0.148.2/hugo_extended_0.125.1_linux-amd64.deb'
+curl -Lo /tmp/hugo.deb 'https://github.com/gohugoio/hugo/releases/download/v0.148.2/hugo_extended_0.148.2_linux-amd64.deb'
 sudo apt-get install -y /tmp/hugo.deb
